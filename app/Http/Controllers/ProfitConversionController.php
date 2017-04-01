30c1cd7a5ae6fd950a5d805509d7875a53801458
Rosataker <pechin1219@gmail.com>
2017-04-01 12:39:52 +0800

    route架構修改

---
+++ app/Http/Controllers/ProfitConversionController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ProfitConversion;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ProfitConversionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('ProfitConversion.view', [
+            'ProfitConversion_Class' => ProfitConversion::orderBy('created_at', 'desc')->paginate(5),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+       return view('ProfitConversion.view',[
+            'status'=>1,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'price' => 'required|numeric',
+            'amount' => 'required|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withInput()
+                ->withErrors($validator);
+        }
+        $ProfitConversionClass = ($request->id) ? ProfitConversion::find($request->id) : new ProfitConversion;
+        $ProfitConversionClass->name = $request->name;
+        $ProfitConversionClass->price = $request->price;
+        $ProfitConversionClass->amount = $request->amount;
+        $ProfitConversionClass->count = ($request->count) ? $request->count : '' ;
+        $ProfitConversionClass->save();
+
+        return redirect()->action('ProfitConversionController@index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('ProfitConversion.view', [
+            'ProfitConversion_edit' => ProfitConversion::where('id', ['id' => $id])->get(),
+            'button_str' => 'Edit ProfitConversion',
+            'status'=>1,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        ProfitConversion::findOrFail($id)->delete();
+        return redirect()->action('ProfitConversionController@index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -10,66 +10,20 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-use App\ProfitConversion;
-use Illuminate\Http\Request;
-Route::group(['middleware' => 'web'], function(){
-
-    Route::get('/ProfitConversion', function (  ) {
-        return view('ProfitConversion.view', [
-            'ProfitConversion_Class' => ProfitConversion::orderBy('created_at', 'desc')->paginate(5),
-        ]);
-    })->name('ProfitConversion_View');
-
-    Route::post('/ProfitConversion', function (Request $request) {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|max:255',
-            'price' => 'required',
-            'amount' => 'required',
-        ]);
-
-        if ($validator->fails()) {
-            return redirect()->route('ProfitConversion_View')
-                ->withInput()
-                ->withErrors($validator);
-        }
-
-        $ProfitConversionClass = ($request->id) ? ProfitConversion::find($request->id) : new ProfitConversion;
-        $ProfitConversionClass->name = $request->name;
-        $ProfitConversionClass->price = $request->price;
-        $ProfitConversionClass->amount = $request->amount;
-        $ProfitConversionClass->count = ($request->count) ? $request->count : '' ;
-        $ProfitConversionClass->save();
-
-        return redirect()->route('ProfitConversion_View');
-    });
-
-    Route::get('/ProfitConversion/add', function () {
-        return view('ProfitConversion.view',[
-            'status'=>1,
-        ]);
-    });
-
-    Route::get('/ProfitConversion/edit/{id}', function ($id) {
-        return view('ProfitConversion.view', [
-            'ProfitConversion_edit' => ProfitConversion::where('id', ['id' => $id])->get(),
-            'button_str' => 'Edit ProfitConversion',
-            'status'=>1,
-        ]);
-    });
+Route::group(['prefix'=>'ProfitConversion'], function()
+{
+    Route::get('/', 'ProfitConversionController@index');
+    Route::get('/add', 'ProfitConversionController@create');
+    Route::get('/edit/{id}', 'ProfitConversionController@edit');
     
-    Route::delete('/ProfitConversion/del/{id}', function ($id) {
-        ProfitConversion::findOrFail($id)->delete();
-        return redirect()->route('ProfitConversion_View');
-    });
-    
+    Route::post('/', 'ProfitConversionController@store');
 
-
+    Route::delete('/del/{id}', 'ProfitConversionController@destroy');
 });
+
+
+
