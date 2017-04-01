48bb34f8afa87f9b2e03b1bb62f6133191592b32
Rosataker <pechin1219@gmail.com>
2017-04-01 12:40:05 +0800

    移除無用

--- app/ProfitConversion.php
+++ app/ProfitConversion.php
@@ -9,13 +9,4 @@ class ProfitConversion extends Model
      protected $table = 'ProfitConversion';
 
 
-/*   
-	#方向錯誤留下來的東西，先留著當參考
-     public function EditView ($id){
-     	$ret = ProfitConversion::where('id', ['id' => $id])->get();
-     //	$ret = "aaaaa";
-     	return $ret;
-     }
-     #'ProfitConversion_edit' =>$ProfitConversionClass->EditView($id)
-*/
 }
