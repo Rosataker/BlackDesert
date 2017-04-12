c9384ceae812877938948f68354999cd2fd1ed5b
Rosataker <pechin1219@gmail.com>
2017-04-12 21:53:21 +0800

    編輯頁動態新增/顯示完成

--- resources/views/ProfitConversion/layout/edit.blade.php
+++ resources/views/ProfitConversion/layout/edit.blade.php
@@ -40,21 +40,22 @@
                         @if (count(@$ProfitConversion_edit) > 0)
                         <div class="form-group">
                             <input type="hidden" name="rawmaterial" value="{{$ProfitConversion_edit->rawmaterial or old('rawmaterial') }}">
-                            <label for="ProfitConversion-rawmaterial" class="col-sm-3 control-label">原料</label>
-                            <div class="col-sm-2">
-                                <input type="text" name="rawmaterial[0][name]" value="{{$rawmaterialData[0]['name']}}" id="ProfitConversion-count" class="form-control" value="">
-                            </div>  
-                            <label for="ProfitConversion-rawmaterial" class="col-sm-2 control-label">價錢</label>
-                            <div class="col-sm-2">
-                                <input type="text" name="rawmaterial[0][price]" value="{{$rawmaterialData[0]['price']}}" id="ProfitConversion-count" class="form-control" value="">
-                            </div>  
+                            <label for="ProfitConversion-rawmaterial" class="col-sm-6 control-label">原料及價錢</label>
+   
                                 <button type="button" id='add_rawmaterial' class="btn btn-primary">
                                     <i class="fa fa-btn fa-plus"></i> Add
                                 </button>
 
                         </div>
                          <div id="showBlock"></div>
+
+                        
+                         @for($i=0; $i < count(@$rawmaterialData); $i++)
+                                <input type="hidden" name="GetRawmaterialData[{{$i}}][name]" value="{{$rawmaterialData[$i]['name']}}">                    
+                                <input type="hidden" name="GetRawmaterialData[{{$i}}][price]" value="{{$rawmaterialData[$i]['price']}}">       
+                         @endfor
                         @endif
+
                         <!-- Add ProfitConversion Button -->
                         <div class="form-group">
                             <div class="col-sm-offset-3 col-sm-6">   
@@ -73,51 +74,46 @@
 <script type="text/javascript">
   var txtId = 0;
     $( document ).ready(function() {
-       // alert({{count(@$rawmaterialData)}});
-     //   var Rawchk=$.isNumeric({{is_array($rawmaterialData)}});
-    // alert({{count(@$rawmaterialData)}});
+
         var RawmateriaDataCount={{count(@$rawmaterialData)}};
         if(RawmateriaDataCount > 1){
-            for ( i = 0; i < RawmateriaDataCount; i++) {
-                if(txtId==i){
-                    continue;
-                }else{
-
-
-
-
-
-
-                     
-                }
-                
+            for ( i = 1; i <= RawmateriaDataCount; i++) {
+                $("#showBlock").append(AddRawmaterialDiv(txtId));
+                txtId=i;
             }
         }
-        
-        
+
+
+
     });
 
-
   $("#add_rawmaterial").click(function () {
-      var add_id='rawmaterial_div' + txtId ;
-      $("#showBlock").append('<div id="' + add_id + '" class="form-group">'
-                +'<label for="ProfitConversion-rawmaterial" class="col-sm-3 control-label">原料</label>'
-                +'<div class="col-sm-2"><input type="text" name="rawmaterial['+txtId+'][name]" value="" id="ProfitConversion-count" class="form-control"></div>'
-                +'<label for="ProfitConversion-rawmaterial" class="col-sm-2 control-label">價錢</label>'
-                +'<div class="col-sm-2"><input type="text" name="rawmaterial['+txtId+'][price]" value="" id="ProfitConversion-count" class="form-control" ></div>'
-          +' <button type="button" id="del_rawmaterial" value="' + add_id +'" class="btn btn-danger"><i class="fa fa-btn fa-ban"></i> Del</button>'
-        +'</div>'
-        );       
-                                
+      $("#showBlock").append(AddRawmaterialDiv(txtId));
       txtId++;
   });
-
 
  $("#showBlock").delegate("#del_rawmaterial", "click", function() {
     var Del_Id='#' + $(this).val();
     $(Del_Id).remove();
  });
 
+function AddRawmaterialDiv(txtId){
+    var ret,GetName , GetPrice;
+    var add_id='rawmaterial_div' + txtId ;
+    GetName = $("input[type=hidden][name='GetRawmaterialData["+txtId+"][name]']").val();
+    GetName = (GetName) ? GetName : "" ;
+    GetPrice = $("input[type=hidden][name='GetRawmaterialData["+txtId+"][price]']").val();
+    GetPrice = (GetPrice) ? GetPrice : "";
+
+    ret ='<div id="' + add_id + '" class="form-group">'
+                +'<label for="ProfitConversion-rawmaterial" class="col-sm-3 control-label">原料</label>'
+                +'<div class="col-sm-2"><input type="text" name="rawmaterial['+txtId+'][name]" value="'+GetName+'" id="ProfitConversion-count" class="form-control"></div>'
+                +'<label for="ProfitConversion-rawmaterial" class="col-sm-2 control-label">價錢</label>'
+                +'<div class="col-sm-2"><input type="text" name="rawmaterial['+txtId+'][price]" value="'+GetPrice+'" id="ProfitConversion-count" class="form-control" ></div>'
+                +' <button type="button" id="del_rawmaterial" value="' + add_id +'" class="btn btn-danger"><i class="fa fa-btn fa-ban"></i> Del</button>'
+        +'</div>';
+    return ret;
+}
 
 </script> 
 
